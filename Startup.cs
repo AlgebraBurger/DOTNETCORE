87cc2593e277b15a0bc2539cb4c025fc501dc9b3
juliusbacosa <juliusbacosa@gmail.com>
2016-09-10 23:22:48 +0800

    - add mvc functionality

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleApplication
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services){
+            services.AddMvc();
+        }
        public void Configure(IApplicationBuilder app)
        {
-            
+            app.Map("/mvc", mvcapp => {
+                mvcapp.UseMvcWithDefaultRoute();
+            });
             app.Run(c => c.Response.WriteAsync("Hello from the end of the pipeline"));
        }
     }
